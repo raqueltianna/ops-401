65ae706fd53d427c99845f459b9d7a5dcb4b9ae0
Tianna Farrow <raqueltianna@outlook.com>
2024-01-09 16:39:26 -0500

    added ops challenge 1 and updated read me

---
+++ uptimesensorpt1.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3 
+
+# Script Name:                          uptimesensorpt1.py
+# Author name:                          Tianna Farrow
+# Date of latest revision:              01/09/2024
+# Purpose:                              Using "if statements" in Python. 
+# Execution:                            python3 uptimesensorpt1.py
+# Additional Resources:                 https://docs.python.org/3/library/subprocess.html; https://stackoverflow.com/questions/2953462/pinging-servers-in-python; https://stackoverflow.com/questions/35750041/check-if-ping-was-successful-using-subprocess-in-python; https://docs.python.org/3/library/datetime.html#strftime-and-strptime-format-codes; https://docs.python.org/3/reference/expressions.html#conditional-expressions; https://docs.python.org/3/reference/lexical_analysis.html#f-strings
+
+
+import subprocess
+import time
+from datetime import datetime   
+
+def ping_host(ip):
+    try:
+        subprocess.check_output(['ping', '-c', '1', ip], stderr=subprocess.STDOUT, text=True)
+        return True
+    except subprocess.CalledProcessError:
+        return False
+    
+def main():
+    ip_address = '192.168.1.62' 
+
+    while True:
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')[:-3]
+        status = "Network Active" if ping_host(ip_address) else "Network Inactive"
+        print(f"{timestamp} {status} to {ip_address}")
+        time.sleep(2)
+
+if __name__ == "__main__":
+    main()
