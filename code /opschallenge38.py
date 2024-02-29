377fd77fe1678a66a3586969c0e13c3537f908aa
Tianna Farrow <147196888+raqueltianna@users.noreply.github.com>
2024-02-29 10:16:55 -0500

    Update opschallenge38.py

--- code /opschallenge38.py
+++ code /opschallenge38.py
@@ -1,6 +1,84 @@
-#!/usr/bin/env python3 
+#!/usr/bin/env python3
 
-# Script Name:                  opschallenge38.py
-# Author Name:                  Tianna Farrow
-# Date of Latest Revision:      02/28/2024
-# Purpose:                      Programmatically detect the presence (or lack) of a XSS vulnerability in a given target URL using a Python script
+# Author:      Abdou Rockikz
+# Description: This script is designed to detect XSS vulnerabilities on a given URL by scanning all forms present on the webpage.
+# Date:        2024-02-28
+# Modified by: Tianna Farrow
+
+### Before executing this script in Python3, make sure to install requests and bs4 ###
+### You can install them using pip: ###
+### pip install requests ###
+### pip install bs4 ###
+
+# Import libraries
+import requests
+from pprint import pprint
+from bs4 import BeautifulSoup as bs
+from urllib.parse import urljoin
+
+# Declare functions
+
+### This function retrieves all forms present on a webpage ###
+def get_all_forms(url):
+    soup = bs(requests.get(url).content, "html.parser")
+    return soup.find_all("form")
+
+### This function retrieves details of a form including action, method, and inputs ###
+def get_form_details(form):
+    details = {}
+    action = form.attrs.get("action").lower()
+    method = form.attrs.get("method", "get").lower()
+    inputs = []
+    for input_tag in form.find_all("input"):
+        input_type = input_tag.attrs.get("type", "text")
+        input_name = input_tag.attrs.get("name")
+        inputs.append({"type": input_type, "name": input_name})
+    details["action"] = action
+    details["method"] = method
+    details["inputs"] = inputs
+    return details
+
+### This function submits a form with specified values ###
+def submit_form(form_details, url, value):
+    target_url = urljoin(url, form_details["action"])
+    inputs = form_details["inputs"]
+    data = {}
+    for input in inputs:
+        if input["type"] == "text" or input["type"] == "search":
+            input["value"] = value
+        input_name = input.get("name")
+        input_value = input.get("value")
+        if input_name and input_value:
+            data[input_name] = input_value
+
+    if form_details["method"] == "post":
+        return requests.post(target_url, data=data)
+    else:
+        return requests.get(target_url, params=data)
+
+### This function scans for XSS vulnerabilities on a given URL ###
+def scan_xss(url):
+    forms = get_all_forms(url)
+    print(f"[+] Detected {len(forms)} forms on {url}.")
+    js_script = '<script>alert("XSS Vulnerability Detected!")</script>'
+    is_vulnerable = False
+    for form in forms:
+        form_details = get_form_details(form)
+        content = submit_form(form_details, url, js_script).content.decode()
+        if js_script in content:
+            print(f"[+] XSS Detected on {url}")
+            print(f"[*] Form details:")
+            pprint(form_details)
+            is_vulnerable = True
+    return is_vulnerable
+
+# Main
+
+### The main function prompts the user to enter a URL and then scans it for XSS vulnerabilities ###
+if __name__ == "__main__":
+    url = input("Enter a URL to test for XSS:") 
+    print(scan_xss(url))
+
+### TODO: When you have finished annotating this script with your own comments, copy it to Web Security Dojo
+### TODO: Test this script against one XSS-positive target and one XSS-negative target
+### TODO: Paste the outputs here as comments in this script, clearling indicating which is positive detection and negative detection
