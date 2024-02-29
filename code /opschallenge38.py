d850bc917391a466c0a95ac44026ba8379a59c90
Tianna Farrow <147196888+raqueltianna@users.noreply.github.com>
2024-02-28 20:28:32 -0500

    Create opschallenge38.py

---
+++ code /opschallenge38.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3 
+
+# Script Name:                  opschallenge38.py
+# Author Name:                  Tianna Farrow
+# Date of Latest Revision:      02/28/2024
+# Purpose:                      Programmatically detect the presence (or lack) of a XSS vulnerability in a given target URL using a Python script
